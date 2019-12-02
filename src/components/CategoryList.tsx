0332537ff423729925e1d62a639b9b34c1788008
christopher.hogg <christopher.hogg@blik.io>
2019-12-02 08:15:31 +0100

    feat: add lint-staged and prettier

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import { Categories, Category } from '../types';
+import React from "react";
+import { Categories, Category } from "../types";
 
-const CategoryList: React.FC<{ categories: Categories, change: Function }> = ({ categories, change }) => {
-    const triviaCategories = categories.trivia_categories
-    const listItems = triviaCategories.map((category: Category) =>
-      <option key={category.id} value={category.id}>{category.name}</option>
-    );
-    return (
-      <React.Fragment>
-        <label htmlFor="pet-select">Choose a Category</label>
-        <select id="pet-select" onChange={e => change(e.target.value)} >
-          {listItems}
-        </select>
-      </React.Fragment>
-    );
-  }
+const CategoryList: React.FC<{ categories: Categories; change: Function }> = ({
+  categories,
+  change
+}) => {
+  const triviaCategories = categories.trivia_categories;
+  const listItems = triviaCategories.map((category: Category) => (
+    <option key={category.id} value={category.id}>
+      {category.name}
+    </option>
+  ));
+  return (
+    <React.Fragment>
+      <label htmlFor="pet-select">Choose a Category</label>
+      <select id="pet-select" onChange={e => change(e.target.value)}>
+        {listItems}
+      </select>
+    </React.Fragment>
+  );
+};
 
-  export default CategoryList
+export default CategoryList;
