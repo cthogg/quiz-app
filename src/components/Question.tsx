3e088efb84c3287ce45f25f41fd51bf93632f00d
christopher.hogg <christopher.hogg@blik.io>
2019-12-02 18:38:42 +0100

    feat: randomise the answers

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import he from "he";
 import { Question } from "../types";
-
+import _ from "lodash";
 const QuestionComp: React.FC<{ question: Question }> = ({ question }) => {
+  const incorrectAnswers = question.incorrect_answers.map(a => ({
+    answer: a,
+    isCorrect: false
+  }));
+  const correctAnswer = { answer: question.correct_answer, isCorrect: true };
+  const allQuestions = incorrectAnswers.concat(correctAnswer);
   return (
     <React.Fragment>
       {question.correct_answer !== undefined && (
         <React.Fragment>
           <p> {he.decode(question.question)} </p>
           <ul>
-            <li>
-              {" "}
-              <em> {question.correct_answer} </em>{" "}
-            </li>
-            {question.incorrect_answers.map((question, index) => (
-              <li key={index}> {question} </li>
-            ))}
+            {_.shuffle(allQuestions).map((question, index) => {
+              return question.isCorrect ? (
+                <em>
+                  {" "}
+                  <li key={index}> {question.answer} </li>{" "}
+                </em>
+              ) : (
+                <li key={index}> {question.answer} </li>
+              );
+            })}
           </ul>
         </React.Fragment>
       )}
